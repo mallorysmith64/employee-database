d5bb23421e36d4842bc1dd2bb3469e14ef0ad36f
Mars <mallorysmith6464@gmail.com>
2019-09-07 19:56:30 -0400

    rendered employee first names on homepage

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -3,17 +3,18 @@ import axios from 'axios'
 // import { setState } from 'expect/build/jestMatchersObject'
 
 const Company = () => {
-  const [employees, setEmployees] = useState('')
+  const [data, setData] = useState([])
 
-  const getCompany = async () => {
-    const resp = await axios.get(
-      'https://sdg-staff-directory-app.herokuapp.com/api/Company/realcompany'
-    )
-    console.log('company api results', resp)
-    console.log('all company results', resp.data)
-    console.log('one company result', resp.data[0])
-    setEmployees(resp.data[0])
-  }
+  // const getCompany = async () => {
+  //   const resp = await axios.get(
+  //     'https://sdg-staff-directory-app.herokuapp.com/api/Company/realcompany'
+  //   )
+  //   console.log('company api results', resp)
+  //   console.log('all company results', resp.data)
+  //   console.log('one company result', resp.data[0])
+  //   console.log('first name:', resp.data[0].firstName)
+  //   setEmployees(resp.data[0].firstName)
+  // }
 
   const getEmployees = async () => {
     const resp = await axios.get(
@@ -22,19 +23,20 @@ const Company = () => {
     console.log('employees', resp)
     console.log('all employees', resp.data)
     console.log('one employee', resp.data[0])
-    // setEmployees(resp.data[0])
+    console.log('first name:', resp.data[0].firstName)
+    setData(resp.data)
   }
 
   useEffect(() => {
-    getCompany()
+    // getCompany()
     getEmployees()
   }, [])
 
   return (
     <>
-      {/* {employees.map((employee, key) => {
-        return <h3 key={key}>{employee}</h3>
-      })} */}
+      {data.map((info, key) => {
+        return <h3 key={key}>{info.firstName}</h3>
+      })}
     </>
   )
 }
