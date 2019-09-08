8710ca1eb94cf2cec27080eff6000175551db658
Mars <mallorysmith6464@gmail.com>
2019-09-08 17:53:26 -0400

    working on employee form

--- src/components/AddEmployee.jsx
+++ src/components/AddEmployee.jsx
@@ -2,14 +2,15 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const AddEmployee = () => {
-  const [newHire] = useState({})
+  const [Hire, setHire] = useState({})
 
   const newEmployee = async () => {
     const resp = await axios.post(
       `https://sdg-staff-directory-app.herokuapp.com/api/realcompany/Employees`,
-      { newHire }
+      { Hire }
     )
-    console.log('employee form', resp.data)
+    console.log('employee form', resp)
+    setHire(resp)
   }
 
   useEffect(() => {
@@ -18,25 +19,38 @@ const AddEmployee = () => {
 
   return (
     <>
-      <h5>New Employee Form</h5>
-      {/* <form>
-        <input type="text" id={firstName} onChange={handleChange} />
-      </form> */}
-      <div class="row">
-        <form class="col s12">
-          <div class="row">
-            <div class="input-field col s6">
-              <input
-                placeholder="Placeholder"
-                id="first_name"
-                type="text"
-                class="validate"
-              />
-              <label for="first_name">First Name</label>
+      {/* <input type="text" id={firstName} onChange={handleChange} /> */}
+
+      <section className="contact-header">
+        <h5>New Employee Form</h5>
+      </section>
+      <section className="form">
+        <div class="row">
+          <form class="col s12">
+            <div class="row">
+              <div class="input-field col s6">
+                <input
+                  placeholder="first name"
+                  id="first_name"
+                  type="text"
+                  // onChange={handleChange}
+                  class="validate"
+                />
+                <label for="first_name">First Name</label>
+              </div>
+              <div class="input-field col s6">
+                <input
+                  placeholder="last name"
+                  id="last_name"
+                  type="text"
+                  class="validate"
+                />
+                <label for="last_name">Last Name</label>
+              </div>
             </div>
-          </div>
-        </form>
-      </div>
+          </form>
+        </div>
+      </section>
     </>
   )
 }
