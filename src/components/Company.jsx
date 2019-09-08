22f4b6f48bd60fff8d3001f9001c21cbb5e726f6
Mars <mallorysmith6464@gmail.com>
2019-09-07 20:26:07 -0400

    added employee information to homepage

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -5,16 +5,16 @@ import axios from 'axios'
 const Company = () => {
   const [data, setData] = useState([])
 
-  // const getCompany = async () => {
-  //   const resp = await axios.get(
-  //     'https://sdg-staff-directory-app.herokuapp.com/api/Company/realcompany'
-  //   )
-  //   console.log('company api results', resp)
-  //   console.log('all company results', resp.data)
-  //   console.log('one company result', resp.data[0])
-  //   console.log('first name:', resp.data[0].firstName)
-  //   setEmployees(resp.data[0].firstName)
-  // }
+  const getCompany = async () => {
+    const resp = await axios.get(
+      'https://sdg-staff-directory-app.herokuapp.com/api/Company/realcompany'
+    )
+    console.log('company api results', resp)
+    console.log('all company results', resp.data)
+    console.log('one company result', resp.data[0])
+    console.log('first name:', resp.data[0].firstName)
+    setData(resp.data)
+  }
 
   const getEmployees = async () => {
     const resp = await axios.get(
@@ -28,14 +28,22 @@ const Company = () => {
   }
 
   useEffect(() => {
-    // getCompany()
+    getCompany()
     getEmployees()
   }, [])
 
   return (
     <>
-      {data.map((info, key) => {
-        return <h3 key={key}>{info.firstName}</h3>
+      {data.map((name, key) => {
+        return (
+          <section key={key}>
+            <h3>
+              Employee Info: {[name.firstName, name.lastName, name.id]}
+              Job Title: {name.jobTitle}
+              Hired Date: {name.hiredDate}
+            </h3>
+          </section>
+        )
       })}
     </>
   )
